7eecb0aa361f099b7d71783d8ce111fd01316aa9
Adam Martinik <a.martinik@seznam.cz>
2012-04-15 17:53:48 +0100

    edit css

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-   before_filter :authorize_user
+  before_filter :authorize_user
    
   private
   def authorize_user
-    @current_user = session[:session_id]
+    @current_user ||= session[:session_id]
   end
 end
