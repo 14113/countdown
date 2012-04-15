7204e599299dc37ead262ca416ecbc75690486b0
Adam Martinik <a.martinik@seznam.cz>
2012-04-15 15:00:40 +0100

    remove js validation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,8 +12,8 @@ Countdown::Application.configure do
   config.serve_static_assets = false
 
   # Compress JavaScripts and CSS
-  #config.assets.compress = true
-
+  config.assets.compress = true
+  #
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
 
